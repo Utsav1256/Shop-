13d0e0c6e3aa97ba52d6b813e8448a51a9a013d4
utsav kumar <utsavkumar5462@gmail.com>
2023-05-18 16:34:18 +0530

    add state to Product

--- src/Product.js
+++ src/Product.js
@@ -1,17 +1,30 @@
 import React from 'react';
 class Product extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      price: 999,
+      title: 'Phone',
+      detail: '4 GB RAM | 64 GB ROM | Expandable Upto 1 TB',
+      img: '',
+    };
+  }
   render() {
+    const { price, title, detail } = this.state;
     return (
       <div className="product">
         <div className="left-block">
           <img style={styles.image} />
         </div>
         <div className="right-block">
-          <div>Phone</div>
-          <div>Rs 999</div>
-          <div>Qty: 1</div>
+          <div>{title}</div>
+          <div>{detail}</div>
+          <div>Rs {price}</div>
         </div>
-        <div className="product-actions">{/* Buttons */}</div>
+        <div className="product-actions">
+          {/* Buttons */}
+          <button className="add-to-cart">Add To Cart</button>
+        </div>
       </div>
     );
   }
